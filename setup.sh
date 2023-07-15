21470005e548b1b92450def960d7dac710394a8e
xing <starlove.xing@gmail.com>
2023-07-15 22:36:32 +0700

    fix setup shell

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+
 # setup tmux config
-ln -s tmux.conf ~/.tmux.conf
+ln -s ${PWD}/tmux.conf ~/.tmux.conf
 # check if .config exists
 if [ ! -d ~/.config ]; then
     mkdir ~/.config
 fi
 
 # setup alacritty config
-ln -s alacritty ~/.config/alacritty
+ln -s ${PWD}/alacritty ~/.config/alacritty
 
 # setup nvim config
 git clone https://github.com/AstroNvim/AstroNvim.git ~/.config/nvim
-ln -s nvim ~/.config/nvim/lua/user
+ln -s ${PWD}/nvim ~/.config/nvim/lua/user
