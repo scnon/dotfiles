b3012c4ea0b6e3c3f1e9e3e3b1d364c74e229ecf
xing <xing@xpc.com>
2023-07-30 18:32:25 +0700

    init window setup script

---
+++ setup.bat
@@ -0,0 +1,7 @@
+
+mklink /J "%APPDATA%\alacritty" "%CD%/alacritty"
+
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim %LocalAppData%\nvim
+
+mklink /J %LocalAppData%\nvim\lua\user "%CD%/nvim"
+mklink /J %UserProfile%\.ideavim %CD%\ideavim
