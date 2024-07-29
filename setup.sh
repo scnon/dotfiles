f272843a94e65e3460bd5a191a0a7d5a7798ce07
x <x@xArch>
2024-07-30 01:32:14 +0700

    add zshrc config

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
 # setup tmux config
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ${PWD}/tmux.conf ~/.tmux.conf
+
 # check if .config exists
 if [ ! -d ~/.config ]; then
     mkdir ~/.config
@@ -13,13 +13,16 @@ fi
 # ln -s ${PWD}/alacritty ~/.config/alacritty
 
 # setup wezterm config 
-# ln -s ${PWD}/wezterm ~/.config/wezterm
+ln -s ${PWD}/wezterm ~/.config/wezterm
 
 # setup nvim config
 git clone https://github.com/AstroNvim/Template.git ~/.config/nvim
 ln -s ${PWD}/nvim ~/.config/nvim/lua/user
 
+# setup zshrc
+echo "source ${PWD}/zshrc">>~/.zshrc
 
+# install deps for nvim plugins
 function install_deps ()
 {
     npm i -g ls_emmet
---
+++ zshrc
@@ -0,0 +1,4 @@
+alias lg=lazygit
+alias n=nvim
+alias vim=nvim
+alias vi=nvim
